4d3f4556ce4a4384034b7436f2e9ec6027f65ba8
Mono-Cloud <44344247+Mono-Cloud@users.noreply.github.com>
2019-10-18 14:56:30 +0100

    Update WindowsUpdater.ps1

--- WindowsUpdater.ps1
+++ WindowsUpdater.ps1
@@ -1,16 +1,16 @@
-Write-Output "[*]Installing Windows Update Module"
+Write-Output "[*] Installing Windows Update Module"
 Install-Module PSWindowsUpdate 
 
 
 Write-Output "[+] Windows Update Module Installed Successfully"
-Start-Sleep -s 1
-Write-Output "[*]Fetching Windows Updates"
+Start-Sleep -s 2
+Write-Output "[*] Fetching Windows Updates"
 Get-WindowsUpdate 
 
 
 Write-Output "[+] Fetched Windows Updates Successfully"
-Start-Sleep -s 1
-Write-Output "[*]Installing Windows Updates"
+Start-Sleep -s 2
+Write-Output "[*] Installing Windows Updates"
 Install-WindowsUpdate
 
 Write-Output "[+] Windows Updates Installed Successfully"
